3d14a2e7bfba7e792f04ebdfd86bb4c9adaa10f6
Adriley francisco almeida pereira <139196116+adrileyf-dev@users.noreply.github.com>
2025-05-28 05:49:57 -0300

    adcionado middlwarmiddlwareme

--- src/index.js
+++ src/index.js
@@ -1,22 +1,48 @@
 const express = require("express");
-const app = express(); ///para teste crie junto emcima
 const { v4: uuidv4 } = require("uuid");
+
+const app = express();
 app.use(express.json());
 
 const customers = [];
+// middlware
+function verifyIfExistsAccountCPF(request, response, next) {
+  const { cpf } = request.params;
+  console.log(cpf);
+  const customer = customers.find((customer) => customer.cpf === cpf);
+  if (!customer) {
+    return response.status(404).json({ error: "Customer not found" });
+  }
+  /// passa para a request par
+  request.customer = customer;
+  return next();
+}
 
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
-  const customersExiste = customers.some((customers) => customers.cpf === cpf);
-  if (customersExiste) {
-    return response.status(400).json({ error: "Customer already existe " });
+
+  const customerExists = customers.some((customer) => customer.cpf === cpf);
+  if (customerExists) {
+    return response.status(400).json({ error: "Customer already exists" });
   }
+
   customers.push({
     cpf,
     name,
     id: uuidv4(),
     statement: [],
   });
-  response.status(201).send();
+
+  return response.status(201).send();
 });
-app.listen(3333);
+
+/// outra forma de usar  middlware  app.user(verifyIfExistsAccountCPF) tudo que tiver abaixo dele vai receber ele
+
+app.get("/statement/:cpf", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+  return response.json(customer.statement);
+});
+
+app.listen(3333, () => {
+  console.log("Servidor rodando na porta 3333");
+});
